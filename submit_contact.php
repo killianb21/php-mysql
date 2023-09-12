594f461cc93206b6a6d0b69b1c948affd5e327b7
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-09-12 16:18:17 +0200

    2eme recuperation des parametre PHP

--- submit_contact.php
+++ submit_contact.php
@@ -25,6 +25,16 @@
 
         <!-- inclusion de l'entête du site -->
         <?php include_once('header.php'); ?>
+   
+        
+        <h1>Message bien reçu !</h1>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Rappel de vos informations</h5>
+                <p class="card-text"><b>Email</b> : <?php echo $_POST['email']; ?></p>
+                <p class="card-text"><b>Message</b> : <?php echo strip_tags($_POST['message']); ?></p>
+            </div>
+        </div>
     </div>
 
     <!-- inclusion du bas de page du site -->
