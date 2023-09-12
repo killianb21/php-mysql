a5e30bc6300d3c8656b1185bdf8402f7dead74c8
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-09-12 16:25:00 +0200

    Testez la présence d'un paramètre

--- submit_contact.php
+++ submit_contact.php
@@ -25,7 +25,17 @@
 
         <!-- inclusion de l'entête du site -->
         <?php include_once('header.php'); ?>
-   
+        
+        <?php
+            if (
+                (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+                || (!isset($_POST['message']) || empty($_POST['message']))
+                )
+            {
+                echo('<h1>Il faut un email et un message valides pour soumettre le formulaire.</h1>');
+                return;
+            }
+        ?>
         
         <h1>Message bien reçu !</h1>
         <div class="card">
