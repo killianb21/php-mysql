68aff153fcc34cf00a9b62bf5a00ff50cb722851
Killian.b <84130010+killianb21@users.noreply.github.com>
2023-09-12 15:48:05 +0200

    Correction TP1

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- footer.php -->
+<!-- footer.php -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2023 Copyright:
+    <a class="text-dark">Killian Bonnet</a>
+  </div>
+</footer>
