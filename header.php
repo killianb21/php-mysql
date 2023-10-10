d8b20c0e70f03f0127f29467a10fad5edf038f27
Killian.b <killian2282@gmail.com>
2023-10-10 11:03:02 +0200

    ajout du raccourci ajouter recette dans le header

--- header.php
+++ header.php
@@ -13,6 +13,9 @@
         <li class="nav-item">
           <a class="nav-link" href="contact.php">Contact</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="ajouter_recette.php">Ajouter Recette</a>
+        </li>
       </ul>
     </div>
   </div>
